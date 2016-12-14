ef905cad992573fdb3f636b97aea8b552426d434
MadeleineLexen <lexen@student.chalmers.se>
2016-12-14 14:23:39 +0100

    added obervable interface

---
+++ src/IObservable.java
@@ -0,0 +1,9 @@
+import java.beans.PropertyChangeListener;
+
+/**
+ * Created by madeleine on 2016-12-14.
+ */
+public interface IObservable {
+    void removeObserver(PropertyChangeListener observer);
+    void addObserver(PropertyChangeListener observer);
+}
