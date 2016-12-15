4b8fce841c0f7f9961f292fb25d13cc46d0161fe
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2016-12-15 08:15:48 +0100

    creates an object of blanktile instead of gametile since it's an interface

--- src/BlankTile.java
+++ src/BlankTile.java
@@ -3,7 +3,7 @@ import java.awt.*;
 /**
  * Created by madeleine on 2016-12-14.
  */
-public class BlankTile implements GameTile1 {
+public class BlankTile implements GameTile {
     @Override
     public void draw(Graphics g, int x, int y, Dimension d) {
 
